cab99732795fcf6521bb7ba554113e731297c347
ReD4rg1 <musenov200228@gmail.com>
2023-10-17 12:35:45 +0600

    Поставил jest, настроил конфиг

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,25 +1,25 @@
-import webpack from 'webpack';
-import {buildWebpackConfig} from "./config/buildWebpackConfig";
-import {BuildEnv, BuildMode, BuildPaths} from "./config/types/config";
+import webpack from "webpack";
+import { buildWebpackConfig } from "./config/build/buildWebpackConfig";
+import { BuildEnv, BuildMode, BuildPaths } from "./config/build/types/config";
 import path from "path";
 
 export default (env: BuildEnv): webpack.Configuration => {
-    const paths: BuildPaths = {
-        entry: path.resolve(__dirname, 'src', 'index.tsx'),
-        build: path.resolve(__dirname, './build'),
-        html: path.resolve(__dirname, 'public', 'index.html'),
-        src: path.resolve(__dirname, 'src'),
-    }
+  const paths: BuildPaths = {
+    entry: path.resolve(__dirname, "src", "index.tsx"),
+    build: path.resolve(__dirname, "./build"),
+    html: path.resolve(__dirname, "public", "index.html"),
+    src: path.resolve(__dirname, "src"),
+  };
 
-    const mode: BuildMode = env.mode || 'development';
-    const port = env.port || 3000;
+  const mode: BuildMode = env.mode || "development";
+  const port = env.port || 3000;
 
-    const isDev = mode === 'development';
+  const isDev = mode === "development";
 
-    return buildWebpackConfig({
-        mode,
-        paths,
-        isDev,
-        port,
-    });
+  return buildWebpackConfig({
+    mode,
+    paths,
+    isDev,
+    port,
+  });
 };
