0031ab6eef630cc878858b1c0d5c436b03b90a48
ReD4rg1 <musenov200228@gmail.com>
2023-11-28 16:44:50 +0600

    Починил storybook

--- src/features/addCommentForm/ui/AddCommentForm/AddCommentForm.stories.tsx
+++ src/features/addCommentForm/ui/AddCommentForm/AddCommentForm.stories.tsx
@@ -2,9 +2,10 @@ import type { Meta, StoryObj } from "@storybook/react";
 import ThemeDecorator from "shared/config/storybook/ThemeDecorator/ThemeDecorator";
 import { Theme } from "app/providers/ThemeProvider";
 import AddCommentForm from "./AddCommentForm";
+import StoreDecorator from "shared/config/storybook/StoreDecorator/StoreDecorator";
 
 const meta = {
-  title: "shared/AddCommentForm",
+  title: "features/AddCommentForm",
   component: AddCommentForm,
   parameters: {
     layout: "centered",
@@ -17,14 +18,29 @@ type Story = StoryObj<typeof meta>;
 
 export const NormalTheme: Story = {
   args: {},
+  decorators: [
+    StoreDecorator({
+      addCommentForm: { text: "", error: "" },
+    }),
+  ],
 };
 
 export const DarkTheme: Story = {
   args: {},
-  decorators: [ThemeDecorator(Theme.DARK)],
+  decorators: [
+    ThemeDecorator(Theme.DARK),
+    StoreDecorator({
+      addCommentForm: { text: "", error: "" },
+    }),
+  ],
 };
 
 export const RedTheme: Story = {
   args: {},
-  decorators: [ThemeDecorator(Theme.RED)],
+  decorators: [
+    ThemeDecorator(Theme.RED),
+    StoreDecorator({
+      addCommentForm: { text: "", error: "" },
+    }),
+  ],
 };
