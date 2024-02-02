b6a97fe86ac547d3dd4df904712ec8376c79a212
ReD4rg1 <musenov200228@gmail.com>
2024-02-02 17:44:21 +0600

    Добавил e2e тесты cypress

--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
@@ -31,6 +31,7 @@ const ArticlesPage = (props: ArticlesPageProps) => {
   return (
     <DynamicModuleLoader reducers={reducers} removeAfterUnmount={false}>
       <Page
+        data-testid="ArticlesPage"
         onScrollEnd={onLoadNextPart}
         className={classNames(cls.wrapper, {}, [className])}
       >
